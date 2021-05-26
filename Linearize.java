36ea288777455ce9aaf6389500ee8f437c4e7de2
Maya <62585570+noob-entity@users.noreply.github.com>
2021-05-26 15:03:57 +0530

    Update Linearize.java

--- Linearize.java
+++ Linearize.java
@@ -17,3 +17,15 @@
       node.children.remove(i+1);
     }
   }
+//efficient approach
+  public static Node linearize2(Node node){
+      if(node.children.size()==0)
+      return node;
+      Node otail= linearize2(node.children.get(node.children.size()-1));
+      for(int i=node.children.size()-2;i>=0;i--){
+      Node tail=linearize2(node.children.get(i));
+      tail.children.add(node.children.get(i+1));
+      node.children.remove(i+1);
+    }
+    return otail;
+  }
