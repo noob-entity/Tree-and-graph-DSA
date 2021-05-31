ed1ae8f05f6c41a92123b7b73205766a36c2a7da
Maya <62585570+noob-entity@users.noreply.github.com>
2021-05-31 12:07:08 +0530

    check if two trees are mirror

---
+++ AreMirror.java
@@ -0,0 +1,13 @@
+ public static boolean areMirror(Node n1, Node n2) {
+       if(n1.children.size()!=n2.children.size())
+      return false;
+    for(int i=0;i<n1.children.size();i++){
+        Node c1=n1.children.get(i);
+         Node c2=n2.children.get(n2.children.size()-i-1);
+         if(!areMirror(c1,c2))
+         return false;
+         
+    }
+    return true;
+    
+  }
