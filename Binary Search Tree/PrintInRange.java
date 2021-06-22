ac3cddcd4892284b934361f91a321321cb10f83f
Maya <62585570+noob-entity@users.noreply.github.com>
2021-06-22 13:25:00 +0530

    Print In Range

---
+++ Binary Search Tree/PrintInRange.java
@@ -0,0 +1,19 @@
+  public static void pir(Node node, int d1, int d2) {
+    // write your code here
+    if(node==null)
+    return;
+   
+    
+    if(node.data>=d1&&node.data<=d2){
+        pir(node.left,d1,d2);
+        System.out.println(node.data);
+          pir(node.right,d1,d2);
+    }
+    else if(node.data>d1&&node.data>d2){
+         pir(node.left,d1,d2);
+    }
+    else{
+         pir(node.right,d1,d2);
+    }
+  
+  }
